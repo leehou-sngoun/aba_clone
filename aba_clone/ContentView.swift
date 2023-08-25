c2ebafb10f98bb58ec925ab23855c10ecdbc21c4
Lyhou Sngoun <lyhou.sngoun@gmail.com>
2023-08-25 18:38:50 +0700

    Initial Commit

---
+++ aba_clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  aba_clone
+//
+//  Created by LEEHOU on 25/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ aba_clone/aba_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  aba_cloneApp.swift
+//  aba_clone
+//
+//  Created by LEEHOU on 25/8/23.
+//
+
+import SwiftUI
+
+@main
+struct aba_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
